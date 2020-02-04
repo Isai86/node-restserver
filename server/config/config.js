728aed4831fff21a7b121b50ca4c648561ef8342
Isai <skatalitico1@hotmail.com>
2020-02-03 19:15:37 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 //========================
 process.env.PORT = process.env.PORT || 3000;
+
+//========================
+// Entorno
+//========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//========================
+// Base de datos
+//========================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/usuarios';
+} else {
+    urlDB = 'mongodb+srv://isai86:K9uRx9np9IdNdR8G@cluster0-n6wvx.mongodb.net/OficiosApp';
+}
+process.env.URLDB = urlDB;
---
+++ server/models/usuario.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+}
+
+const Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} email ya esta ocupado, intenta con otro o recupera tu contraseña' })
+
+module.exports = mongoose.model('Contratistas', usuarioSchema);
