4693a225482ba4e651cd923879692620c54e8c54
Isai <skatalitico1@hotmail.com>
2020-02-11 18:29:35 -0600

    Login normal section 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,12 +3,15 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const _ = require('underscore');
 
-const Usuario = require('../models/usuario');
+const Contratistas = require('../models/usuario');
+
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 const app = express();
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
+
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -16,7 +19,7 @@ app.get('/usuario', function(req, res) {
     let limite = req.query.limite || 5;
     limite = Number(limite);
 
-    Usuario.find({ estado: true }, 'nombre email role estado google img')
+    Contratistas.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
         .limit(limite)
         .exec((err, usuarios) => {
@@ -28,7 +31,7 @@ app.get('/usuario', function(req, res) {
                 });
             }
 
-            Usuario.count({ estado: true }, (err, conteo) => {
+            Contratistas.count({ estado: true }, (err, conteo) => {
 
                 res.json({
                     ok: true,
@@ -41,11 +44,11 @@ app.get('/usuario', function(req, res) {
 
     //res.json('get Usuario LOCAL');
 });
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
 
-    let usuario = new Usuario({
+    let usuario = new Contratistas({
         nombre: body.nombre,
         email: body.email,
         password: bcrypt.hashSync(body.password, 10),
@@ -70,11 +73,11 @@ app.post('/usuario', function(req, res) {
     });
 
 });
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
-    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+    Contratistas.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
 
         if (err) {
             return res.status(400).json({
@@ -85,13 +88,13 @@ app.put('/usuario/:id', function(req, res) {
 
         res.json({
             ok: true,
-            Usuario: usuarioDB
+            usuario: usuarioDB
         });
     });
 
 
 });
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
 
@@ -99,7 +102,7 @@ app.delete('/usuario/:id', function(req, res) {
             estado: false
         }
         //Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
-    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+    Contratistas.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
 
         if (err) {
             return res.status(400).json({
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -12,12 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(require('./routes/usuario'));
+//configuracion global de rutas
+app.use(require('./routes/index'));
+
 
 mongoose.connect(process.env.URLDB, {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify: false
     },
     (err, res) => {
         if (err) throw err;
